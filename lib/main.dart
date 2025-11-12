d7bd6fcd2630f2864507c84d012f96275a343b05
paladin-2024 <cnzabb@gmail.com>
2025-11-12 23:36:51 +0300

    refactor: Standardize project name and update dependencies
    This commit standardizes the project name to lowercase "kubochain" in `package-lock.json` and moves `splash_screen.dart` into a new `screens` directory.
    
    Key changes:
    - Renames project from "KuboChain" to "kubochain" in package lock files.
    - Updates the import path for `splash_screen.dart` in `main.dart` to reflect its new location.
    - Bumps versions for `meta` and `test_api` in `pubspec.lock`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kubochain/splash_screen.dart';
-
+import 'package:kubochain/screens/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
