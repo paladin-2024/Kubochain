4643defe35a4818d227268345de6cbad5a4a594d
Caleb <cnzabb@gmail.com>
2026-05-19 19:16:08 +0300

    chore: suppress Java 8 obsolete source/target compiler warnings
      Force Java 11 sourceCompatibility and targetCompatibility across all
      subprojects in root build.gradle.kts to silence warnings from
      third-party plugins still defaulting to Java 8.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,10 @@ allprojects {
         google()
         mavenCentral()
     }
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_11.toString()
+        targetCompatibility = JavaVersion.VERSION_11.toString()
+    }
 }
 
 val newBuildDir: Directory =
